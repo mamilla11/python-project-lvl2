f0ca75acdd335df1b5a67f8a29be6ad79d7f5284
Anastasiia Shishova <shishova@arrival.com>
2021-10-13 23:03:36 +0300

    project setup

---
+++ Makefile
@@ -0,0 +1,23 @@
+install:
+	poetry install
+
+build:
+	poerty build
+
+publish:
+	poetry publish --dry-run
+
+package-install:
+	python -m pip install --user dist/*.whl --force-reinstall
+
+lint:
+	poetry run flake8 gendiff
+
+test:
+	poetry run pytest
+
+test-coverage:
+	poetry run pytest --cov=gendiff --cov-report html
+
+diff:
+	poetry run gendiff --help
---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('first_file', metavar='first_file')
+    parser.add_argument('second_file', metavar='second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
+
+    parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
