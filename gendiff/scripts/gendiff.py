02c6b27546909f87a7787bb355edad6645b752fd
Anastasiia Shishova <shishova@arrival.com>
2021-10-15 22:52:05 +0300

    differ refactoring

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -21,55 +21,50 @@ def load_files(file1, file2):
 
 
 def stylish(items):
-    def inner(items, ident, diffstr):
-        diffstr += '{\n'
-
-        for key in items:
-            diffstr += ' ' * (ident + 1)
-            if isinstance(items[key], dict):
-                diffstr += f'{key}: '
-                diffstr += inner(items[key], ident + 2, '')
-            else:
-                for val in items[key]:
-                    diffstr += f'{val[0]} {key}: {val[1]}\n'
-        diffstr += ' ' * (ident) + '}\n'
-        return diffstr
-
-    return inner(items, 0, '')
+    pass
 
 
 def get_status(old, new, key):
+    status = ''
+
     if key in new and key not in old:
-        return 'added'
+        status = 'added'
 
-    if key in old and key not in new:
-        return 'deleted'
+    elif key in old and key not in new:
+        status = 'deleted'
 
-    if isinstance(old[key], dict) and isinstance(new[key], dict):
-        return 'nested'
+    elif isinstance(old[key], dict) and isinstance(new[key], dict):
+        status = 'nested'
 
-    if old[key] != new[key]:
-        return 'changed'
+    elif old[key] != new[key]:
+        status = 'changed'
 
-    return 'unchanged'
+    else:
+        status = 'unchanged'
+
+    return status
 
 
 def get_diff(old, new, key):
     status = get_status(old, new, key)
+    result = []
 
     if status == 'added':
-        return [(key, '+', new[key])]
+        result = [(key, '+', new[key])]
 
-    if status == 'deleted':
-        return [(key, '-', old[key])]
+    elif status == 'deleted':
+        result = [(key, '-', old[key])]
 
-    if status == 'nested':
-        return [(key, ' ', generate_diff(old[key], new[key]))]
+    elif status == 'nested':
+        result = [(key, ' ', generate_diff(old[key], new[key]))]
 
-    if status == 'changed':
-        return [(key, '-', old[key]), (key, '+', new[key])]
+    elif status == 'changed':
+        result = [(key, '-', old[key]), (key, '+', new[key])]
 
-    return [(key, ' ', new[key])]
+    else:
+        result = [(key, ' ', new[key])]
+
+    return result
 
 
 def generate_diff(old, new):
