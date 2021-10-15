487ba54f750db0bae95e1ea762ac11dbb62484e0
Anastasiia Shishova <shishova@arrival.com>
2021-10-15 22:44:39 +0300

    tests added

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,25 +1,54 @@
+import os
 import pytest
 from gendiff.scripts.gendiff import generate_diff
 
 
-@pytest.fixture(scope="module", autouse=True)
-def expected():
-    with open('tests/fixtures/result.txt') as f:
+def get_path(filename):
+    os.path.join('tests/fixtures', filename)
+
+
+@pytest.fixture
+def expected_plain():
+    with open(get_path('result_plain.txt') as f:
         expected = f.read()
     return expected
 
 
-def test_generate_diff_json(expected):
-    diff = generate_diff('tests/fixtures/file1.json', 'tests/fixtures/file2.json')
+@pytest.fixture
+def expected_nested():
+    with open(get_path('result_nested.txt') as f:
+        expected = f.read()
+    return expected
+
+
+def test_generate_diff_json(expected_plain):
+    diff = generate_diff(get_path('plain1.json', get_path('plain2.json')))
     assert diff == expected
 
-    diff = generate_diff('tests/fixtures/file2.json', 'tests/fixtures/file1.json')
+    diff = generate_diff(get_path('plain2.json', get_path('plain1.json')))
     assert diff != expected
 
 
-def test_generate_diff_yaml(expected):
-    diff = generate_diff('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml')
+def test_generate_diff_yaml(expected_plain):
+    diff = generate_diff(get_path('plain1.yml', get_path('plain2.yml')))
     assert diff == expected
 
-    diff = generate_diff('tests/fixtures/file2.yml', 'tests/fixtures/file1.yml')
+    diff = generate_diff(get_path('plain2.yml', get_path('plain1.yml')))
     assert diff != expected
+
+
+def test_generate_diff_nested_json(expected_nested):
+    diff = generate_diff(get_path('nested1.json', get_path('nested2.json')))
+    assert diff == expected
+
+    diff = generate_diff(get_path('nested2.json', get_path('nested1.json')))
+    assert diff != expected
+
+
+def test_generate_diff_nested_json(expected_nested):
+    diff = generate_diff(get_path('nested1.yml', get_path('nested2.yml')))
+    assert diff == expected
+
+    diff = generate_diff(get_path('nested2.yml', get_path('nested1.yml')))
+    assert diff != expected
+
