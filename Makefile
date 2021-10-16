ef71917292e1a958e23d99c6b8fc6bcc2af7d070
Anastasiia Shishova <shishova@arrival.com>
2021-10-16 03:11:51 +0300

    tests updated

--- Makefile
+++ Makefile
@@ -14,10 +14,13 @@ lint:
 	poetry run flake8 gendiff
 
 test:
-	poetry run pytest
+	poetry run pytest -v -vv
 
 test-coverage:
 	poetry run pytest --cov=gendiff --cov-report xml
 
 diff:
-	poetry run gendiff tests/fixtures/file1.json tests/fixtures/file2.json
+	poetry run gendiff tests/fixtures/nested1.yml tests/fixtures/nested2.yml
+
+diff_plain:
+	poetry run gendiff tests/fixtures/plain1.json tests/fixtures/plain2.json
--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,54 +1,82 @@
 import os
+import json
+import yaml
 import pytest
-from gendiff.scripts.gendiff import generate_diff
+
+from gendiff.scripts.gendiff import stylish
+from gendiff.differ import generate_diff
 
 
 def get_path(filename):
-    os.path.join('tests/fixtures', filename)
+    return os.path.join('tests/fixtures', filename)
 
 
 @pytest.fixture
 def expected_plain():
-    with open(get_path('result_plain.txt') as f:
+    with open(get_path('result_plain.txt')) as f:
         expected = f.read()
-    return expected
+    return expected.strip()
 
 
 @pytest.fixture
 def expected_nested():
-    with open(get_path('result_nested.txt') as f:
+    with open(get_path('result_nested.txt')) as f:
         expected = f.read()
-    return expected
+    return expected.strip(' ')
 
 
 def test_generate_diff_json(expected_plain):
-    diff = generate_diff(get_path('plain1.json', get_path('plain2.json')))
-    assert diff == expected
+    filepath1 = get_path('plain1.json')
+    filepath2 = get_path('plain2.json')
 
-    diff = generate_diff(get_path('plain2.json', get_path('plain1.json')))
-    assert diff != expected
+    file1 = json.load(open(filepath1))
+    file2 = json.load(open(filepath2))
+
+    diff = generate_diff(file1, file2)
+    assert stylish(diff) == expected_plain
+
+    diff = generate_diff(file2, file1)
+    assert stylish(diff) != expected_plain
 
 
 def test_generate_diff_yaml(expected_plain):
-    diff = generate_diff(get_path('plain1.yml', get_path('plain2.yml')))
-    assert diff == expected
+    filepath1 = get_path('plain1.yml')
+    filepath2 = get_path('plain2.yml')
 
-    diff = generate_diff(get_path('plain2.yml', get_path('plain1.yml')))
-    assert diff != expected
+    file1 = yaml.safe_load(open(filepath1))
+    file2 = yaml.safe_load(open(filepath2))
 
+    diff = generate_diff(file1, file2)
+    assert stylish(diff) == expected_plain
 
-def test_generate_diff_nested_json(expected_nested):
-    diff = generate_diff(get_path('nested1.json', get_path('nested2.json')))
-    assert diff == expected
-
-    diff = generate_diff(get_path('nested2.json', get_path('nested1.json')))
-    assert diff != expected
+    diff = generate_diff(file2, file1)
+    assert stylish(diff) != expected_plain
 
 
 def test_generate_diff_nested_json(expected_nested):
-    diff = generate_diff(get_path('nested1.yml', get_path('nested2.yml')))
-    assert diff == expected
+    filepath1 = get_path('nested1.json')
+    filepath2 = get_path('nested2.json')
 
-    diff = generate_diff(get_path('nested2.yml', get_path('nested1.yml')))
-    assert diff != expected
+    file1 = json.load(open(filepath1))
+    file2 = json.load(open(filepath2))
+
+    diff = generate_diff(file1, file2)
+    assert stylish(diff) == expected_nested
+
+    diff = generate_diff(file2, file1)
+    assert stylish(diff) != expected_nested
+
+
+def test_generate_diff_nested_yaml(expected_nested):
+    filepath1 = get_path('nested1.yml')
+    filepath2 = get_path('nested2.yml')
+
+    file1 = yaml.safe_load(open(filepath1))
+    file2 = yaml.safe_load(open(filepath2))
+
+    diff = generate_diff(file1, file2)
+    assert stylish(diff) == expected_nested
+
+    diff = generate_diff(file2, file1)
+    assert stylish(diff) != expected_nested
 
